e797ade9c854d7076481cfc72da0262eaf8f99e5
ophayden <haydenkk13@gmail.com>
2020-10-29 12:44:22 -0600

    uc2

--- UART_232/src/UART_rx.v
+++ UART_232/src/UART_rx.v
@@ -33,21 +33,13 @@ end
 
 
 
-////////////////////////////////////////////////////////////////////////////
-////////////////////////////Next step decision//////////////////////////////
-////////////////////////////////////////////////////////////////////////////
-/*This is easy. Each time "State or Rx or RxEn or RxDone" will change their value
-we decide which is the next step. 
-	- Obviously we get to IDLE only when RxDone is high
-meaning that the read process is done.
-	- Also, while we are into IDEL, we get to READ state only if Rx input gets low meaning
-we've detected a start bit*/
+
 always @ (State or Rx or RxEn or RxDone)
 begin
     case(State)	
-	IDLE:	if(!Rx & RxEn)		Next = READ;	 //If Rx is low (Start bit detected) we start the read process
+	IDLE:	if(!Rx & RxEn)		Next = READ;	 
 		else			Next = IDLE;
-	READ:	if(RxDone)		Next = IDLE; 	 //If RxDone is high, than we get back to IDLE and wait for Rx input to go low (start bit detect)
+	READ:	if(RxDone)		Next = IDLE; 	 
 		else			Next = READ;
 	default 			Next = IDLE;
     endcase
@@ -58,18 +50,16 @@ end
 
 
 
-////////////////////////////////////////////////////////////////////////////
-///////////////////////////ENABLE READ OR NOT///////////////////////////////
-////////////////////////////////////////////////////////////////////////////
+
 always @ (State or RxDone)
 begin
     case (State)
 	READ: begin
-		read_enable <= 1'b1;			//If we are in the Read state, we enable the read process so in the "Tick always" we start getting the bits
+		read_enable <= 1'b1;			
 	      end
 	
 	IDLE: begin
-		read_enable <= 1'b0;			//If we get back to IDLE, we desable the read process so the "Tick always" could continue without geting Rx bits
+		read_enable <= 1'b0;			
 	      end
     endcase
 end
@@ -78,43 +68,35 @@ end
 
 
 
-////////////////////////////////////////////////////////////////////////////
-///////////////////////////Read the input data//////////////////////////////
-////////////////////////////////////////////////////////////////////////////
-/*Finally, each time we detect a Tick pulse,we increase a couter.
-- When the counter is 8 (4'b1000) we are in the middle of the start bit
-- When the counter is 16 (4'b1111) we are in the middle of one of the bits
-- We store the data by shifting the Rx input bit into the Read_data register 
-using this line of code: Read_data <= {Rx,Read_data[7:1]};
-*/
+
 always @ (posedge Tick)
 
 	begin
 	if (read_enable)
 	begin
-	RxDone <= 1'b0;							//Set the RxDone register to low since the process is still going
-	counter <= counter+1;						//Increase the counter by 1 with each Tick detected
+	RxDone <= 1'b0;							
+	counter <= counter+1;						
 	
 
-	if ((counter == 4'b1000) & (start_bit))				//Counter is 8? Then we set the start bit to 1. 
+	if ((counter == 4'b1000) & (start_bit))				
 	begin
 	start_bit <= 1'b0;
 	counter <= 4'b0000;
 	end
 
-	if ((counter == 4'b1111) & (!start_bit) & (Bit < NBits))	//We make a loop (8 loops in this case) and we read all 8 bits
+	if ((counter == 4'b1111) & (!start_bit) & (Bit < NBits))	
 	begin
 	Bit <= Bit+1;
 	Read_data <= {Rx,Read_data[7:1]};
 	counter <= 4'b0000;
 	end
 	
-	if ((counter == 4'b1111) & (Bit == NBits)  & (Rx))		//Then we count to 16 once again and detect the stop bit (Rx input must be high)
+	if ((counter == 4'b1111) & (Bit == NBits)  & (Rx))		
 	begin
 	Bit <= 4'b0000;
 	RxDone <= 1'b1;
 	counter <= 4'b0000;
-	start_bit <= 1'b1;						//We reset all values for next data input and set RxDone to high
+	start_bit <= 1'b1;						
 	end
 	end
 	
--- UART_232/src/UART_tx.v
+++ UART_232/src/UART_tx.v
@@ -1,77 +1,63 @@
 `timescale 1ns / 1ps
-module UART_rs232_tx (Clk,Rst_n,TxEn,TxData,TxDone,Tx,Tick,NBits);	//Define my module as UART_rs232_tx
+module UART_rs232_tx (Clk,Rst_n,TxEn,TxData,TxDone,Tx,Tick,NBits);	
 
-input Clk, Rst_n, TxEn,Tick;	//Define 1 bit inputs
-input [3:0]NBits;		//Define 4 bits inputs
-input [7:0]TxData;		//Define 8 bit inputs
-
+input Clk, Rst_n, TxEn,Tick;	
+input [3:0]NBits;		
+input [7:0]TxData;		
 output Tx;
 output TxDone;
 
 
 
 
-//Variabels used for state machine...
-parameter  IDLE = 1'b0, WRITE = 1'b1;	//We have 2 states for the State Machine state 0 and 1 (WRITE adn IDLE)
-reg  State, Next;			//Create some registers for the states
-reg  TxDone = 1'b0;			//Variable used to notify when the transmission process is done
-reg  Tx;				//We register the input value
-reg write_enable = 1'b0;		//Variable used to activate or deactivate the transmission process			
-reg start_bit = 1'b1;			//Variable used to notify if the START bit was made or not yet
-reg stop_bit = 1'b0;			//Variable used to notify if the STOP bit was made or not yet
-reg [4:0] Bit = 5'b00000;		//Variable used for the bit by bit write loop (in this case 8 bits so 8 loops)
-reg [3:0] counter = 4'b0000;		//Counter variable used to count the tick pulses up to 16
-reg [7:0] in_data=8'b00000000;		//Register where we store tha data that arrived with the TxData input and has to be sent
-reg [1:0] R_edge;			//Variable used to avoid debounce of the write enable pin
-wire D_edge;				//Wire used to connect the D_edge
+parameter  IDLE = 1'b0, WRITE = 1'b1;	
+reg  State, Next;			
+reg  TxDone = 1'b0;			
+reg  Tx;				
+reg write_enable = 1'b0;	
+reg start_bit = 1'b1;	
+reg stop_bit = 1'b0;	
+reg [4:0] Bit = 5'b00000;		
+reg [3:0] counter = 4'b0000;		
+reg [7:0] in_data=8'b00000000;		
+reg [1:0] R_edge;			
+wire D_edge;				
 
 
 
-///////////////////////////////STATE MACHINE////////////////////////////////
-////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////Reset////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////
-always @ (posedge Clk or negedge Rst_n)			//It is good to always have a reset always
+
+always @ (posedge Clk or negedge Rst_n)			
 begin
-if (!Rst_n)	State <= IDLE;				//If reset pin is low, we get to the initial state which is IDLE
-else 		State <= Next;				//If not we go to the next state
+if (!Rst_n)	State <= IDLE;		
+else 		State <= Next;	
 end
 
 
 
 
-////////////////////////////////////////////////////////////////////////////
-////////////////////////////Next step decision//////////////////////////////
-////////////////////////////////////////////////////////////////////////////
-/* This is easy as well.  Each time "State or D_edge or TxData or TxDone" will 
-change their value we decide which is the next step. 
- - If D_edge was detected, so the TxEn was enabeled, we start the write process
- - Obviously, if the TxDone is high, then we get back to IDLE and wait for next TxEn to be activated */
+
 always @ (State or D_edge or TxData or TxDone) 
 begin
     case(State)	
-	IDLE:	if(D_edge)		Next = WRITE;		//If we are into IDLE and D_edge gets activated, we start the WRITE process
+	IDLE:	if(D_edge)		Next = WRITE;		
 		else			Next = IDLE;
-	WRITE:	if(TxDone)		Next = IDLE;  		//If we are into WRITE and TxDone gets high, we get back to IDLE and wait
-		else			Next = WRITE;
+	WRITE:	if(TxDone)		Next = IDLE;  		
 	default 			Next = IDLE;
     endcase
 end
 
 
 
-////////////////////////////////////////////////////////////////////////////
-///////////////////////////ENABLE WRITE OR NOT//////////////////////////////
-////////////////////////////////////////////////////////////////////////////
+
 always @ (State)
 begin
     case (State)
 	WRITE: begin
-		write_enable <= 1'b1;	//If we are in the WRITE state, we enable the write process
+		write_enable <= 1'b1;	
 	end
 	
 	IDLE: begin
-		write_enable <= 1'b0;	//If we are in the IDLE state, we disable the write process
+		write_enable <= 1'b0;
 	end
     endcase
 end
@@ -80,38 +66,29 @@ end
 
 
 
-////////////////////////////////////////////////////////////////////////////
-///////////////////////Write the data out on Tx pin/////////////////////////
-////////////////////////////////////////////////////////////////////////////
-/*Finally, each time we detect a Tick pulse, if the write_enable is enabeled,
-we start counting ticks. First we set the Tx pin to LOW and that indicates a start bit.
-Then each 16 ticks, we set the Tx output to a value acording to the "in_data" value which
-is the data to eb sent. We do that by shifting the "in_data" using this lines: 
-	in_data <= {1'b0,in_data[7:1]};
-	Tx <= in_data[0]; */
+
 
 always @ (posedge Tick)
 begin
 
-	if (!write_enable)				//if write_enable is not activated, then we reset all varaibles for enxt loop
-	begin
+	if (!write_enable)				
 	TxDone = 1'b0;
 	start_bit <=1'b1;
 	stop_bit <= 1'b0;
 	end
 
-	if (write_enable)				//if write_enable is activated, then we start counting and changing the Tx output
+	if (write_enable)				
 	begin
-	counter <= counter+1;				//Increase the counter by one each positive edge of the Tick input
+	counter <= counter+1;				
 	
 	
-	if(start_bit & !stop_bit)			//We set the Tx to LOW (start bit) and pass the TxData input to the in:data register
+	if(start_bit & !stop_bit)			
 	begin
-	Tx <=1'b0;					//Create start bit  (low pulse)
-	in_data <= TxData;				//Pass the data to eb sent to the in_data register so we could use it
+	Tx <=1'b0;					
+	in_data <= TxData;		
 	end		
 
-	if ((counter == 4'b1111) & (start_bit) )	//If counter reaches 16 (4'b1111), then we create the first bit and set "start_bit" to low
+	if ((counter == 4'b1111) & (start_bit) )	
 	begin		
 	start_bit <= 1'b0;
 	in_data <= {1'b0,in_data[7:1]};
@@ -119,7 +96,7 @@ begin
 	end
 
 
-	if ((counter == 4'b1111) & (!start_bit) &  (Bit < NBits-1))	//If we reach 16 once again, we make a loop for the next 7 bits (NBits-1)
+	if ((counter == 4'b1111) & (!start_bit) &  (Bit < NBits-1))	
 	begin		
 	in_data <= {1'b0,in_data[7:1]};
 	Bit<=Bit+1;
@@ -131,14 +108,14 @@ begin
 
 
 	
-	if ((counter == 4'b1111) & (Bit == NBits-1) & (!stop_bit))	//We finish, so we set Tx to HIGH (Stop bit)
+	if ((counter == 4'b1111) & (Bit == NBits-1) & (!stop_bit))	
 	begin
 	Tx <= 1'b1;	
 	counter <= 4'b0000;	
 	stop_bit<=1'b1;
 	end
 
-	if ((counter == 4'b1111) & (Bit == NBits-1) & (stop_bit) )	//If stop bit was enabeled, than we reset the values and wait for enxt write process
+	if ((counter == 4'b1111) & (Bit == NBits-1) & (stop_bit) )	
 	begin
 	Bit <= 4'b0000;
 	TxDone <= 1'b1;
